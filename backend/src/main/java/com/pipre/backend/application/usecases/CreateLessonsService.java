0e738e05307f63a164fa147bb1da11a5b34a0560
YoshuaPariona <loganpariona.1207@gmail.com>
2026-05-06 01:39:56 +0000

    fix: Actualizar endpoint de lesson

---
+++ backend/src/main/java/com/pipre/backend/application/usecases/CreateLessonsService.java
@@ -0,0 +1,27 @@
+package com.pipre.backend.application.usecases;
+
+import com.pipre.backend.adapters.in.web.dto.LessonRequestDTO;
+import com.pipre.backend.application.ports.input.CreateLessonsUseCase;
+import com.pipre.backend.application.ports.output.LessonRepositoryPort;
+import com.pipre.backend.domain.entities.Lesson;
+import com.pipre.backend.domain.factories.LessonFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateLessonsService implements CreateLessonsUseCase {
+    private final LessonRepositoryPort lessonRepositoryPort;
+
+    @Override
+    public void execute(LessonRequestDTO requestDTO) {
+        Lesson newLesson = LessonFactory.createNewLesson(
+                requestDTO.title(),
+                null,
+                null,
+                null,
+                null
+        );
+        lessonRepositoryPort.save(newLesson);
+    }
+}
---
+++ backend/src/main/java/com/pipre/backend/application/usecases/GetLessonsService.java
@@ -0,0 +1,26 @@
+package com.pipre.backend.application.usecases;
+
+import com.pipre.backend.adapters.in.web.dto.LessonResponseDTO;
+import com.pipre.backend.application.ports.input.GetLessonsUseCase;
+import com.pipre.backend.application.ports.output.LessonRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetLessonsService implements GetLessonsUseCase {
+    private final LessonRepositoryPort lessonRepositoryPort;
+
+    @Override
+    public List<LessonResponseDTO> execute(String idModule) {
+        return lessonRepositoryPort.findAll()
+                .stream()
+                .map(lesson -> new LessonResponseDTO(
+                        lesson.getIdLesson(),
+                        lesson.getTitle()
+                ))
+                .toList();
+    }
+}
