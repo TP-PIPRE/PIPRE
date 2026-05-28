c28159aa0280b7a0a40d49ee68bce96deea5db34
YoshuaPariona <loganpariona.1207@gmail.com>
2026-05-28 14:35:37 +0000

    feat: Preparar dominio para endpoints de mvp 2

---
+++ backend/src/main/java/com/pipre/backend/domain/entities/Ranking.java
@@ -0,0 +1,86 @@
+package com.pipre.backend.domain.entities;
+
+public class Ranking {
+
+    private final String idRanking;
+    private final Integer totalPoints;
+    private final Integer position;
+    private final String idGroup;
+    private final String idStudent;
+
+    Ranking(String idRanking, Integer totalPoints, Integer position, String idGroup, String idStudent) {
+        this.idRanking = idRanking;
+        this.totalPoints = totalPoints;
+        this.position = position;
+        this.idGroup = idGroup;
+        this.idStudent = idStudent;
+    }
+
+    public static RankingBuilder builder() {
+        return new RankingBuilder();
+    }
+
+    public String getIdRanking() {
+        return this.idRanking;
+    }
+
+    public Integer getTotalPoints() {
+        return this.totalPoints;
+    }
+
+    public Integer getPosition() {
+        return this.position;
+    }
+
+    public String getIdGroup() {
+        return this.idGroup;
+    }
+
+    public String getIdStudent() {
+        return this.idStudent;
+    }
+
+    public static class RankingBuilder {
+        private String idRanking;
+        private Integer totalPoints;
+        private Integer position;
+        private String idGroup;
+        private String idStudent;
+
+        RankingBuilder() {
+        }
+
+        public RankingBuilder idRanking(String idRanking) {
+            this.idRanking = idRanking;
+            return this;
+        }
+
+        public RankingBuilder totalPoints(Integer totalPoints) {
+            this.totalPoints = totalPoints;
+            return this;
+        }
+
+        public RankingBuilder position(Integer position) {
+            this.position = position;
+            return this;
+        }
+
+        public RankingBuilder idGroup(String idGroup) {
+            this.idGroup = idGroup;
+            return this;
+        }
+
+        public RankingBuilder idStudent(String idStudent) {
+            this.idStudent = idStudent;
+            return this;
+        }
+
+        public Ranking build() {
+            return new Ranking(this.idRanking, this.totalPoints, this.position, this.idGroup, this.idStudent);
+        }
+
+        public String toString() {
+            return "Ranking.RankingBuilder(idRanking=" + this.idRanking + ", totalPoints=" + this.totalPoints + ", position=" + this.position + ", idGroup=" + this.idGroup + ", idStudent=" + this.idStudent + ")";
+        }
+    }
+}
