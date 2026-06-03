57700fbd142f94793bbc770bd9c852b317eec050
YoshuaPariona <loganpariona.1207@gmail.com>
2026-06-03 19:50:06 +0000

    build: Añadir seeders de Simulations

--- backend/src/main/java/com/pipre/backend/adapters/out/persistence/mapper/SimulationMapper.java
+++ backend/src/main/java/com/pipre/backend/adapters/out/persistence/mapper/SimulationMapper.java
@@ -1,6 +1,8 @@
 package com.pipre.backend.adapters.out.persistence.mapper;
 
+import com.pipre.backend.adapters.out.persistence.jpaEntities.ActivityJpaEntity;
 import com.pipre.backend.adapters.out.persistence.jpaEntities.SimulationJpaEntity;
+import com.pipre.backend.adapters.out.persistence.jpaEntities.UserJpaEntity;
 import com.pipre.backend.domain.entities.Simulation;
 
 public class SimulationMapper {
@@ -9,6 +11,17 @@ public class SimulationMapper {
         SimulationJpaEntity entity = new SimulationJpaEntity();
         entity.setIdSimulation(domain.getIdSimulation());
         entity.setResult(domain.getResult());
+        if (domain.getIdStudent() != null) {
+            UserJpaEntity studentEntity = new UserJpaEntity();
+            studentEntity.setIdUser(domain.getIdStudent());
+            entity.setStudentJpaEntity(studentEntity);
+        }
+
+        if (domain.getIdActivity() != null) {
+            ActivityJpaEntity activityEntity = new ActivityJpaEntity();
+            activityEntity.setIdActivity(domain.getIdActivity());
+            entity.setActivityJpaEntity(activityEntity);
+        }
         return entity;
     }
     public static com.pipre.backend.domain.entities.Simulation toDomain(SimulationJpaEntity entity) {
--- backend/src/main/java/com/pipre/backend/domain/entities/Simulation.java
+++ backend/src/main/java/com/pipre/backend/domain/entities/Simulation.java
@@ -1,10 +1,5 @@
 package com.pipre.backend.domain.entities;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Builder
-@Getter
 public class Simulation {
 
     private final String idSimulation;
@@ -12,4 +7,68 @@ public class Simulation {
     private final String idStudent;
     private final String idActivity;
 
+    Simulation(String idSimulation, String result, String idStudent, String idActivity) {
+        this.idSimulation = idSimulation;
+        this.result = result;
+        this.idStudent = idStudent;
+        this.idActivity = idActivity;
+    }
+
+    public static SimulationBuilder builder() {
+        return new SimulationBuilder();
+    }
+
+    public String getIdSimulation() {
+        return this.idSimulation;
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public String getIdStudent() {
+        return this.idStudent;
+    }
+
+    public String getIdActivity() {
+        return this.idActivity;
+    }
+
+    public static class SimulationBuilder {
+        private String idSimulation;
+        private String result;
+        private String idStudent;
+        private String idActivity;
+
+        SimulationBuilder() {
+        }
+
+        public SimulationBuilder idSimulation(String idSimulation) {
+            this.idSimulation = idSimulation;
+            return this;
+        }
+
+        public SimulationBuilder result(String result) {
+            this.result = result;
+            return this;
+        }
+
+        public SimulationBuilder idStudent(String idStudent) {
+            this.idStudent = idStudent;
+            return this;
+        }
+
+        public SimulationBuilder idActivity(String idActivity) {
+            this.idActivity = idActivity;
+            return this;
+        }
+
+        public Simulation build() {
+            return new Simulation(this.idSimulation, this.result, this.idStudent, this.idActivity);
+        }
+
+        public String toString() {
+            return "Simulation.SimulationBuilder(idSimulation=" + this.idSimulation + ", result=" + this.result + ", idStudent=" + this.idStudent + ", idActivity=" + this.idActivity + ")";
+        }
+    }
 }
